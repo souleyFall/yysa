26a01e525998b001169295a4962cda57be3a25f7
souleyFall <129006034+souleyFall@users.noreply.github.com>
2025-04-06 17:00:06 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-let header = document.querySelector("header");
+document.getElementById('overlay').style.display='none';
+resetInputs();
+let header = document.querySelector("header")
+let data_successfully_saved=
+`
+<CENTER>
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+    <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
+    </svg>
+<CENTER>
+<h2>Votre demande a bien été transmis</h2>
+<h3>Merci à vous</h3>
+
+`;
 let header_mobile=
 `
 <div class="header-mobile">
@@ -67,8 +81,6 @@ let header_laptop=
         <!------------- Fin section logo-nav ------------>
 `;
 
-document.getElementById('overlay').style.display='none';
-
 function close_form(){
     document.getElementById('overlay').style.display='none';
 }
@@ -99,3 +111,28 @@ function aosInit() {
     });
 }
 window.addEventListener('load', aosInit);
+
+function sendEmail() {
+    fetch("http://localhost:5000/send-email", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ 
+        PRENOM : document.getElementById("prenom").value, 
+        NOM : document.getElementById("nom").value, 
+        EMAIL : document.getElementById("mail-in-popup").value, 
+        TEL : document.getElementById("tel").value }),
+    })
+    .then((res) => res.json())
+    .then((data) => alert("données enregistrées"))
+    .catch((err) => console.error("Erreur:", err)); 
+    document.querySelector("form").innerHTML=data_successfully_saved;
+}
+
+function resetInputs(){
+    const inputs = document.querySelectorAll("input");
+    inputs.forEach(input => {
+      input.value = ""; 
+    });
+}
+  
+
